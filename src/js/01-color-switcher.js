86e5e1fde21ae057d63a448aa030340b309b6458
DiSBond <87495312+DiSBond@users.noreply.github.com>
2022-01-13 20:06:40 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,46 @@
 
+
+const refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector("button[data-start]"),
+  stopBtn: document.querySelector("button[data-stop]")
+}
+
+let timer = null;
+let counter = 1;
+
+const randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const TIME = 1000;
+
+refs.startBtn.addEventListener('click', startBtnClick)
+refs.stopBtn.addEventListener('click', stopBtnClick)
+
+function startBtnClick() {
+  if (counter <= 1) {
+    refs.body.style.backgroundColor = randomColor();
+  timer = setInterval(() => {
+    refs.body.style.backgroundColor = randomColor();
+  }, TIME);
+    counter += 1;
+    return console.log("started changing color");
+  }
+return console.log("second");
+}
+
+function stopBtnClick() {
+  clearInterval(timer)
+  counter = 0;
+}
+
+refs.startBtn.style.width = "300px";
+refs.startBtn.style.height = "100px";
+
+
+refs.stopBtn.style.width = "300px";
+refs.stopBtn.style.height = "100px";
+
+
+
